1332dfa8b08773a12a08bf6d7db7a51b5add3315
Jonathan Willian Moreira da Cruz <jonathan.cruz@idealinvest.com.br>
2017-11-30 14:37:04 -0200

    .

--- src/mock/postContratos.js
+++ src/mock/postContratos.js
@@ -2,7 +2,8 @@ import fetch from 'node-fetch';
 
 import json from '../../4-5k_propostas';
 
-const main = () => fetch('http://localhost:8080/api/contratos', {
+const main = (name, value) => 
+fetch('http://10.10.3.160:8080/api/contratos', {
   method: 'POST',
   body: JSON.stringify(json),
   headers: {
@@ -10,7 +11,14 @@ const main = () => fetch('http://localhost:8080/api/contratos', {
   },
 })
   .then(res => res.json())
-  .then(res => console.log(res))
+  .then(res => {
+    console.log('Thread: %s - Contagem: %d', name, value)
+    if(value < 25)
+      main(name, ++value);
+  })
   .catch(err => console.error(err));
 
-main();
+main('a', 1);
+main('b', 1);
+main('c', 1);
+main('d', 1);
